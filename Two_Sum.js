29ea42fc064a19807eebcfbc6c748aae789a78ea
Irving <Irvingb232@gmail.com>
2016-11-27 13:22:50 -0800

    [add] two sums description

---
+++ Two_Sum.js
@@ -0,0 +1,8 @@
+/*
+  You are given an array of numbers and a target value. Return the indices of the first two numbers within the array that add up to the target value as a tuple, and an empty array if no such numbers exist.
+
+  Input:
+    ([1,2,3,4], 5) 
+  Output:
+    should return [1,2] rather than [0,3].
+*/
