cefdc05e3ffce7c373299ef75866251fe447144b
Irving <Irvingb232@gmail.com>
2016-11-28 14:10:00 -0800

    [refactor] missing semi colons

--- Array_Max.js
+++ Array_Max.js
@@ -13,11 +13,11 @@ function max (numbers) {
   
   for (var i= 0; i < numbers.length; i++) {
     if (largest === null) {
-      largest = numbers[i]
+      largest = numbers[i];
     } 
     
     if (numbers[i] > largest) {
-      largest = numbers[i]
+      largest = numbers[i];
     }
   }
   
