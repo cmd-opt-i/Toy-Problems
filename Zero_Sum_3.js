8a4bd80853458d91f0b071d73ba265346bc0c980
Irving <Irvingb232@gmail.com>
2016-11-28 15:35:29 -0800

    [add] zero sum 3 solution

--- Zero_Sum_3.js
+++ Zero_Sum_3.js
@@ -5,5 +5,27 @@
     [1, 3, 2, -3]
   Output:
     True
-
+        
 */
+
+function zeroSum3 (numbers) {
+  if (numbers.length < 3) {
+    return false;
+  }
+  var first = 0, second = 0, third = 0;
+  
+  for (var index1 = 0; index1 < numbers.length - 2; index1++) {
+    first = numbers[index1];
+    for (var index2 = index1 + 1; index2 < numbers.length - 1; index2++) {
+      second = numbers[index2];
+      for (var index3 = index2 + 1; index3 < numbers.length; index3++) {
+        third = numbers[index3];
+        if (first + second + third === 0) {
+          return true;
+        }
+      }
+    }  
+  }
+  
+  return false;
+}
