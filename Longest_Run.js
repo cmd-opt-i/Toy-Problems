f25d1828607ac46834d322dc825862fcd8fb283a
Irving <Irvingb232@gmail.com>
2016-11-29 11:06:32 -0800

    [add] longest run solution

--- Longest_Run.js
+++ Longest_Run.js
@@ -8,3 +8,35 @@
   Output:
     [1, 3]
 */
+
+function longestRun (string) {
+  var longestCounter = 0, currCounter = 0;
+  var longestRunArr = [0, 0];
+  var sLetter = null, eLetter = null;
+  var sIndex = null, eIndex = null;
+  
+  for (var i = 0; i < string.length; i++) {
+    var currLetter = string[i];
+
+    if (i === 0) {
+      sLetter = string[i];
+      sIndex = 0;
+    } else if (currLetter === sLetter) {
+      eLetter = currLetter;
+      eIndex = i;
+      currCounter++;
+
+      if (currCounter > longestCounter) {
+        longestCounter = currCounter;
+        longestRunArr[0] = sIndex;
+        longestRunArr[1] = eIndex;
+      }
+    } else {
+      currCounter = 0;
+      sLetter = string[i];
+      sIndex = i;
+    }
+  }
+    
+  return longestRunArr;
+}
