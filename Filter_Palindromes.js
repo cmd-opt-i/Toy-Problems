6e4cf904143aed81ac4b2a2fcadce26c707b2dc9
Irving <Irvingb232@gmail.com>
2016-12-01 17:09:40 -0800

    [add] filter palindrome solution

--- Filter_Palindromes.js
+++ Filter_Palindromes.js
@@ -7,3 +7,28 @@
   Output:
     ["Ana", "race car"]
 */
+
+function filterPalindromes (words) {
+  var palindromes = words.filter(function(word) {
+    if (isPalindrome(word)) {
+      return word;
+    }
+  });
+  
+  return palindromes;
+}
+
+function isPalindrome (input) {
+  var word = input.replace(/\s+/g, '');
+  var back = 0, front = word.length - 1;
+
+  while (back <= front) {
+    if (word[back].toLowerCase() !== word[front].toLowerCase()) {
+      return false;
+    }
+    back++;
+    front--;
+  }
+  
+  return true;
+}
